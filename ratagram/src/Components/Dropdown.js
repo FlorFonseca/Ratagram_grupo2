c9246fc9688b1f7e66ec6abec0351fd606762012
nachopinar <136358447+nachopinar@users.noreply.github.com>
2024-11-10 19:53:55 -0300

    anda bien get users, filtra bien por username

--- ratagram/src/Components/Dropdown.js
+++ ratagram/src/Components/Dropdown.js
@@ -1,9 +1,16 @@
 import * as React from "react";
 import { useNavigate } from "react-router-dom";
 
+const token = localStorage.getItem("token");
+
 const getUsers = async () => {
-  const usersFetch = await fetch("http://localhost:3001/api/all");
+  const usersFetch = await fetch("http://localhost:3001/api/user/all", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
   const users = await usersFetch.json();
+  console.log(users);
   return users;
 };
 
@@ -24,7 +31,7 @@ const Dropdown = () => {
   React.useEffect(() => {
     setFilteredUsers(
       users.filter((user) =>
-        user.name.toLowerCase().includes(searchTerm.toLowerCase())
+        user.username.toLowerCase().includes(searchTerm.toLowerCase())
       )
     );
   }, [searchTerm, users]);
